a46edbb81fa5f07cc722cff25bd92cb2d63ba805
안영민 <aem9704@khu.ac.kr>
2020-07-06 21:18:07 +0900

    Practice template string

---
+++ syntax/template.js
@@ -0,0 +1,5 @@
+var author = 'ph-2';
+var letter = `Dear ${author} Lorem ipsum dolor ${author} sit am
+et, consectetur adipisicing elit. ${author} Ab adipisci alias assumenda ${1+1} distinctio explicabo iste iure minus nisi praesentium sequi? Alias doloribus est facere non odit omnis rem veritatis ${author} voluptatum.`;
+console.log(letter);
+
