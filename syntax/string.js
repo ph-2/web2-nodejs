e9ddea38dc570873eb440196122f1cd003e5c7bd
안영민 <aem9704@khu.ac.kr>
2020-07-06 21:07:22 +0900

    Practice string data type and length function

---
+++ syntax/string.js
@@ -0,0 +1,9 @@
+// string + string = nested string
+console.log('1'+'1');
+
+// string + number -> number will be casted as a string. (and nested string)
+console.log('1'+1);
+
+// int length(str)
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius et fugiat nemo neque perspiciatis quidem quis! Dolorum ex expedita ipsa neque officia pariatur praesentium quam quasi qui ratione rerum, temporibus'.length);
+
