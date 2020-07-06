74237ca07f6d8765bd6b83197c9609a58ff0925f
안영민 <aem9704@khu.ac.kr>
2020-07-06 21:12:29 +0900

    Practice variable

---
+++ syntax/variable.js
@@ -0,0 +1,3 @@
+var author = 'ph-2';
+var letter = 'Dear ' + author + ' Lorem ipsum dolor ' + author + ' sit amet, consectetur adipisicing elit. ' + author + ' Ab adipisci alias assumenda ' + author + ' distinctio explicabo iste iure minus nisi praesentium sequi? Alias doloribus est facere non odit omnis rem veritatis ' + author + ' voluptatum.';
+
